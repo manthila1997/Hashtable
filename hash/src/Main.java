84824bcb9c610f8030e00d59178ecc61deaebdda
manthila1997 <49613631+manthila1997@users.noreply.github.com>
2020-11-24 18:50:01 +0530

    adding data to the hash table done

--- hash/src/Main.java
+++ hash/src/Main.java
@@ -6,9 +6,27 @@ public class Main {
     public static  void main(String[] args){
         Scanner s = new Scanner(System.in);
         hash c = new hash();
-        String a = s.next();
-       int x =  c.calHash(a);
-       c.hashTable(x);
+        char y;
+        int x =0;
+        String data=null;
+        c.hashTable();
+        String[] arr = {"mango", "apple", "banana","coffee","manthila","mango"};
+
+       do {
+//           String a = s.next();
+           for(int i=0;i<arr.length;i++){
+             c.calHash(arr[i]);
+               c.addData(arr[i]);
+
+           }
+
+           c.print();
+
+           System.out.println("do you want to add another value");
+            y = s.next().charAt(0);
+
+       }
+       while (y == ('y'));
 
 
 
--- hash/src/hash.java
+++ hash/src/hash.java
@@ -2,6 +2,10 @@ import java.lang.reflect.Array;
 import java.util.LinkedList;
 
 public class hash {
+    private int position=0;
+    LinkedList[] hash = new LinkedList[9];
+
+
     public int calHash(String key){
         int hash=0;
         int tableSize = 9;
@@ -13,31 +17,39 @@ public class hash {
 
         }
          hash = tot%tableSize;
+        this.position = hash;
         return hash;
     }
 
-    public void hashTable(int x){
-        int[] hashKey = new int[9];
-        LinkedList[] hash = new LinkedList[9];
-
-
+    public void hashTable(){
         for (int i=0; i<9 ; i++){
             hash[i] = new LinkedList();
 
         }
+    }
+
+    public void addData(String data){
         for (int i=0; i<9 ; i++){
-            if(i==x){
-                hash[i].addFirst("hello");
-                hash[i].add("babe");
+            if(i==position){
+                if (hash[position].isEmpty()){
+                    hash[i].addFirst(data);
+
+                }else{
+                    hash[position].add(data);
+
+                }
             }
 
         }
+
+    }
+
+    public void print(){
         for (int i=0; i<9 ; i++){
 
             System.out.println(hash[i]);
 
 
         }
-
     }
 }
